f230cffada57a0bb98ed0a9350e79d6d74ef0ae4
rdhammack88 <rdhammack88@hotmail.com>
2018-11-14 23:24:00 -0600

    Created interfaces for individual movies, and movie lists.

---
+++ src/app/interfaces/movie-list.ts
@@ -0,0 +1,8 @@
+export interface MovieList {
+	dates?: Object;
+	page?: string;
+	movies?: Object;
+	totalPages?: string;
+	totalResults?: string;
+	moviePoster?: string;
+}
---
+++ src/app/interfaces/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+	backdrop_path?: string | null;
+	poster_path?: string | null;
+	original_title?: string;
+	overview?: string;
+	title?: string;
+	release_date?: string;
+	backdropUrl?: string;
+	posterUrl?: string;
+}
