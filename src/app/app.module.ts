e3115ff74336f06a46824f94779acf54fab6a24a
rdhammack88 <rdhammack88@hotmail.com>
2018-11-16 00:00:13 -0600

    Imported MovieService, SummaryPipe and HomeComponent. Added new imports to @NgModule Metadata.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movies/movie/movie.component';
 import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
+import { MovieService } from './services/movie.service';
+import { SummaryPipe } from './pipes/summary.pipe';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-	declarations: [AppComponent, MovieComponent, MovieListComponent],
+	declarations: [AppComponent, MovieComponent, MovieListComponent, SummaryPipe, HomeComponent],
 	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-	providers: [],
+	providers: [MovieService],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
