729bb6d759ece3ec831f7a59879db5876b7cda8c
Dustin <rdhammack88@hotmail.com>
2018-12-06 00:12:26 -0600

    Removed app title and moved to navigation.component.ts file. Also have added test logic currently commented out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movie-reviewer';
+	// @Input() searchType: string;
+	// searchTypes: Object = {
+	// 	theater: 'nowPlaying',
+	// 	popMovies: 'popularMovies',
+	// 	popTv: 'popularTv',
+	// 	upcoming: 'upcoming'
+	// };
+
+	// constructor() {
+	// 	console.log(this.searchType);
+	// }
 }
