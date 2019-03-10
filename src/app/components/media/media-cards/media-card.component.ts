72921df30b19c53122d35f56c4ee0b5985327e9a
Dustin <rdhammack88@hotmail.com>
2019-03-10 05:18:30 -0500

    Commented out all previously used code, that was generated for the "media details" page, that has been moved to its own component - "media-details". (Still need to move all related logic for each media card into component)

--- src/app/components/media/media-cards/media-card.component.ts
+++ src/app/components/media/media-cards/media-card.component.ts
@@ -12,40 +12,40 @@ import { Media } from 'src/app/interfaces/media';
 	styleUrls: ['./media-card.component.scss']
 })
 export class MediaCardComponent implements OnInit {
-	mediaId: string;
-	mediaData: Media;
-	mediaType: string;
-	constructor(
-		private mediaService: MediaService,
-		private route: ActivatedRoute,
-		private location: Location) {
-		// console.log(this.route.snapshot.paramMap.get('id'));
-		// this.mediaId = this.route.snapshot.paramMap.get('id');
-		// this.mediaType = this.route.snapshot.paramMap.get('mediaType');
-		// console.log(this.mediaId);
-		// this.getMediaDetails();
-	}
+	// mediaId: string;
+	// mediaData: Media;
+	// mediaType: string;
+	// constructor(
+	// 	private mediaService: MediaService,
+	// 	private route: ActivatedRoute,
+	// 	private location: Location) {
+	// 	// console.log(this.route.snapshot.paramMap.get('id'));
+	// 	// this.mediaId = this.route.snapshot.paramMap.get('id');
+	// 	// this.mediaType = this.route.snapshot.paramMap.get('mediaType');
+	// 	// console.log(this.mediaId);
+	// 	// this.getMediaDetails();
+	// }
 
 	ngOnInit() {
-		this.mediaId = this.route.snapshot.paramMap.get('id');
-		this.mediaType = this.route.snapshot.paramMap.get('mediaType');
-		this.getMediaDetails(this.mediaType, this.mediaId);
+	// 	this.mediaId = this.route.snapshot.paramMap.get('id');
+	// 	this.mediaType = this.route.snapshot.paramMap.get('mediaType');
+	// 	this.getMediaDetails(this.mediaType, this.mediaId);
 	}
 
-	/** Get Media */
-	getMediaDetails(url: string, mediaId = '') {
-		this.mediaService.getMediaDetails(url, mediaId).subscribe(media => {
-			console.log(media);
-			this.mediaData = media;
-			console.log(this.mediaData);
-		}),
-			err => {
-				console.log(err)
-				// this.mediaService.getMediaDetails('movie', this.mediaId).subscribe(media => {
-				// 	console.log(media);
-				// 	this.mediaData = media;
-				// 	console.log(this.mediaData);
-				// });
-			};
-	}
+	// /** Get Media */
+	// getMediaDetails(url: string, mediaId = '') {
+	// 	this.mediaService.getMediaDetails(url, mediaId).subscribe(media => {
+	// 		console.log(media);
+	// 		this.mediaData = media;
+	// 		console.log(this.mediaData);
+	// 	}),
+	// 		err => {
+	// 			console.log(err)
+	// 			// this.mediaService.getMediaDetails('movie', this.mediaId).subscribe(media => {
+	// 			// 	console.log(media);
+	// 			// 	this.mediaData = media;
+	// 			// 	console.log(this.mediaData);
+	// 			// });
+	// 		};
+	// }
 }
