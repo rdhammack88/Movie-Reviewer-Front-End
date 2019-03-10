c9e20d9ff405b10fcc79c2afff700dd963cecd73
Dustin <rdhammack88@hotmail.com>
2019-03-10 05:02:31 -0500

    Moved the "media-details" page into its own component, was previously being called from the "media-card" component.

---
+++ src/app/components/media/media-details/media-details.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { MediaService } from 'src/app/services/media.service';
+import { Media } from 'src/app/interfaces/media';
+
+@Component({
+  selector: 'media-details',
+  templateUrl: './media-details.component.html',
+  styleUrls: ['./media-details.component.scss']
+})
+export class MediaDetailsComponent implements OnInit {
+	mediaId: string;
+	mediaData: Media;
+	mediaType: string;
+	constructor(
+		private mediaService: MediaService,
+		private route: ActivatedRoute,
+		private location: Location) {
+		// console.log(this.route.snapshot.paramMap.get('id'));
+		// this.mediaId = this.route.snapshot.paramMap.get('id');
+		// this.mediaType = this.route.snapshot.paramMap.get('mediaType');
+		// console.log(this.mediaId);
+		// this.getMediaDetails();
+	}
+
+	ngOnInit() {
+		this.mediaId = this.route.snapshot.paramMap.get('id');
+		this.mediaType = this.route.snapshot.paramMap.get('mediaType');
+		this.getMediaDetails(this.mediaType, this.mediaId);
+	}
+
+	/** Get Media */
+	getMediaDetails(url: string, mediaId = '') {
+		this.mediaService.getMediaDetails(url, mediaId).subscribe(media => {
+			console.log(media);
+			this.mediaData = media;
+			console.log(this.mediaData);
+		}),
+			err => {
+				console.log(err)
+				// this.mediaService.getMediaDetails('movie', this.mediaId).subscribe(media => {
+				// 	console.log(media);
+				// 	this.mediaData = media;
+				// 	console.log(this.mediaData);
+				// });
+			};
+	}
+}
