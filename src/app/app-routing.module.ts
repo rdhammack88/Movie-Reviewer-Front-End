5b499b8ceaf6e70dc422983dee84247f7468f0b6
Dustin <rdhammack88@hotmail.com>
2019-03-10 05:04:44 -0500

    Changed route to media-details page to call "MediaDetails" component, instead of "MediaCard" component. Commented out the theater/:page route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 /** Import of Components */
 import { HomeComponent } from './components/home/home.component';
 import { MediaListComponent } from './components/media/media-list/media-list.component';
-import { MediaCardComponent } from './components/media/media-cards/media-card.component';
+import { MediaDetailsComponent } from './components/media/media-details/media-details.component';
+// import { MediaCardComponent } from './components/media/media-cards/media-card.component';
 import { AboutComponent } from './components/about/about.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { TvListComponent } from './components/tv-list/tv-list.component';
@@ -15,10 +16,10 @@ const routes: Routes = [
 	{ path: 'movies', component: MoviesListComponent },
 	{ path: 'tv', component: TvListComponent },
 	{ path: 'about', component: AboutComponent },
-	{ path: ':mediaType/:id', component: MediaCardComponent },
+	{ path: ':mediaType/:id', component: MediaDetailsComponent },
 	{ path: ':mediaType/:mediaUrl/page/:page', component: MediaListComponent },
 
-	{ path: 'theater/:page', component: MediaListComponent },
+	// { path: 'theater/:page', component: MediaListComponent },
 	{ path: '**', component: NotFoundComponent}
 ];
 
